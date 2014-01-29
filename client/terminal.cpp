dabd2f8c098d888414e7a9a01a07b03cdcc01e44
unknown <c_bode@JUMBLE.ENCS.concordia.ca>
2014-01-29 16:19:46 -0500

    got file list working for client

--- client/terminal.cpp
+++ client/terminal.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 Terminal::Terminal() {
-
 }
 
 /**
@@ -11,20 +10,32 @@ Terminal::Terminal() {
  */
 void Terminal::run() {
 
+	// Set current directory
+	if( ! _getcwd(directory, sizeof(directory)) ) {
+		
+		// If we can't set the current directory, quit
+		cout << "ERROR: could not retrieve working directory. \n\r Quitting..";
+		Sleep(1000);
+		exit(0);
+	}
+
+	// Terminal ready
+	cout << "\n\r Ready.. \n\r\n\r";
+
 	// Wait for commands until quit
 	while(true) {
 
 		// Wait for input
-		cout << "waiting for input: ";
 		char input[128] = "";
 		cin >> input;
 		cout << "\n\r";
 
 		// Process input
 		if( ! process(input) ) {
-			cout << "input not understood!";
-			cout << "\n\r";
+			cout << "input not understood! \n\r";
 		}
+		
+		cout << "\n\r";
 	}
 }
 
@@ -44,7 +55,54 @@ bool Terminal::process(const char * input) {
 		Sleep(1000);
 
 		// Exit program
-		std::exit(0);
+		exit(0);
+	}
+
+	// If the user wants to list local resources
+	else if(strcmp(input, "list") == 0) {
+
+		// Print initial message
+		cout << "Files in directory \"" << directory << "\": \n\r";
+
+		// Open directory
+		if ((dir = opendir(directory)) != NULL) {
+		  
+		  // Iterate through the directory
+		  while ((dirEntry = readdir(dir)) != NULL) {
+
+			  struct stat st;
+
+			  // Check if entry is a file or folder
+			  stat(dirEntry->d_name, &st);
+			  
+			  // If it is a folder
+			  if(S_ISDIR(st.st_mode)) {
+
+				  // Do nothing.
+			  }
+
+			  // If it is a file
+			  else {
+
+				  // Print the name
+				  cout << "- " << dirEntry->d_name << "\n\r";
+			  }
+		  }
+		  closedir (dir);
+		} 
+		
+		// Failed to open directory
+		else {
+		  
+		  // say bye and quit!
+		  cout << "Failed to open directory :( \n\r Quitting..";
+		  Sleep(1000);
+		  exit(0);
+		}
+
+		cout << "\n\r";
+		
+		return true;
 	}
 
 	// If the input is not understood
--- client/terminal.h
+++ client/terminal.h
@@ -2,12 +2,23 @@
 #define TERMINAL_H
 #include <iostream>
 #include <windows.h>
+#include <string>
+#include <direct.h>
+#include "../lib/dirent.h"
+#include <sys/stat.h>
+
 
 class Terminal {
 public:
+	// functions
 	Terminal();
 	void run();
 	bool process(const char *);
+
+	// variables
+	char directory[FILENAME_MAX];
+	DIR *dir;
+	struct dirent *dirEntry;
 };
 
 #endif
